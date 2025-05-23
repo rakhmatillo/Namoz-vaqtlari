734344a7f3fef4d3e3555650496386d90beac45c
rakhmatillo <rakhmatillo.topiboldiev@gmail.com>
2025-05-23 18:14:32 +0500

    Initial Commit

---
+++ Namoz vaqtlari/Namoz_vaqtlariApp.swift
@@ -0,0 +1,17 @@
+//
+//  Namoz_vaqtlariApp.swift
+//  Namoz vaqtlari
+//
+//  Created by rakhmatillo on 23/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Namoz_vaqtlariApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
